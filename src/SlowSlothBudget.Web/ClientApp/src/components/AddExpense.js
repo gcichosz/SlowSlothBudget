3d9c587d81d6b38bbbc7d650e607f1201d7da2e7
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-08-31 16:57:59 +0200

    Create date input

--- src/SlowSlothBudget.Web/ClientApp/src/components/AddExpense.js
+++ src/SlowSlothBudget.Web/ClientApp/src/components/AddExpense.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AmountInput from './AmountInput';
 import auth0Client from '../utils/Auth';
+import DateInput from "./DateInput";
 
 const AddExpense = () => (
     <div>
         <h1>Welcome to Slow Sloth Budget</h1>
         {auth0Client.isAuthenticated() ? <h2>Anonymous users shouldn't see this</h2> : ''}
         <AmountInput />
+        <DateInput />
     </div>
 );
 
---
+++ src/SlowSlothBudget.Web/ClientApp/src/components/DateInput.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import DateInput from "./DateInput";
+
+it('replaces \',\' characters with \'.\'', () => {
+    let commas = ',,,';
+    let dots = '...';
+    const event = {
+        preventDefault() {},
+        target: { value: commas }
+    };
+    const wrapper = mount(<DateInput />);
+
+    wrapper.find('input').simulate('change', event);
+    
+    expect(wrapper.find('input').props().value).toEqual(dots);
+});
