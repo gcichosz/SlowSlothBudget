9111e23c82213177a9f43a00370a36e4dbe27211
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-09-05 15:54:24 +0200

    Add MongoDB configuration

---
+++ src/SlowSlothBudget.Web/Options/MongoDbOptions.cs
@@ -0,0 +1,9 @@
+namespace SlowSlothBudget.Web.Options
+{
+    public class MongoDbOptions
+    {
+        public string Server { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
--- src/SlowSlothBudget.Web/Startup.cs
+++ src/SlowSlothBudget.Web/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SlowSlothBudget.Web.Options;
 
 namespace SlowSlothBudget.Web
 {
@@ -20,6 +21,8 @@ namespace SlowSlothBudget.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MongoDbOptions>(Configuration.GetSection("MongoDB"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
