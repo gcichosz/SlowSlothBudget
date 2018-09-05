aa9a4c1c0b57f3ce1449c00ad4496ed511043e37
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-09-05 16:21:13 +0200

    Implement basic add expense

---
+++ src/SlowSlothBudget.Web/Controllers/ExpensesController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using SlowSlothBudget.Web.DAL;
+using SlowSlothBudget.Web.Models;
+
+namespace SlowSlothBudget.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly IExpensesRepository _expensesRepository;
+
+        public ExpensesController(IExpensesRepository expensesRepository)
+        {
+            _expensesRepository = expensesRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateExpense(Expense expense)
+        {
+            var createdExpense = _expensesRepository.Create(expense);
+            return CreatedAtAction(nameof(GetExpense), new {id = createdExpense.Id.ToString()}, createdExpense);
+        }
+
+        [HttpGet]
+        public IActionResult GetExpense(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/SlowSlothBudget.Web/Models/Expense.cs
@@ -0,0 +1,18 @@
+using System;
+using MongoDB.Bson;
+
+namespace SlowSlothBudget.Web.Models
+{
+    public class Expense
+    {
+        public ObjectId Id { get; set; }
+        
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+    }
+}
