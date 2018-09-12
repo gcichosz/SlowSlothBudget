6b0b1816327fd4e0c13c60c0abe352542974e362
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-09-12 19:19:58 +0200

    Handle add expense API responses

--- src/SlowSlothBudget.Web/ClientApp/src/components/AddExpense.js
+++ src/SlowSlothBudget.Web/ClientApp/src/components/AddExpense.js
@@ -18,7 +18,14 @@ class AddExpense extends React.Component {
                 category: '',
                 description: '',
             },
-            submitted: false
+            submitted: false,
+            loading: false,
+            saved: false,
+            errors: {
+                unauthorized: false,
+                serverError: false,
+                badRequestError: false
+            }
         };
 
         this.handleAmountChange = this.handleAmountChange.bind(this);
@@ -58,13 +65,15 @@ class AddExpense extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
 
-        this.setState({submitted: true});
+        const cleanErrors = {};
+        this.setState({submitted: true, saved: false, errors: cleanErrors});
 
         const expense = this.state.expense;
         if (!expense.amount || !expense.date || !expense.category) {
             return;
         }
 
+        this.setState({loading: true});
         fetch('/api/expenses', {
             method: 'POST',
             headers: {
@@ -73,7 +82,34 @@ class AddExpense extends React.Component {
                 'Authorization': `Bearer ${auth0Client.getIdToken()}`
             },
             body: JSON.stringify(expense)
-        });
+        }).then(response => {
+            if (response.ok) {
+                const cleanExpense = {
+                    amount: '',
+                    date: moment(),
+                    category: '',
+                    description: ''
+                };
+                this.setState({expense: cleanExpense, submitted: false, saved: true})
+            } else {
+                throw new Error(response.status)
+            }
+        }).catch(error => {
+            switch (error.message) {
+                case '400':
+                    const badRequestError = {badRequest: true};
+                    this.setState({errors: badRequestError});
+                    break;
+                case '401':
+                    const unauthorizedError = {unauthorized: true};
+                    this.setState({errors: unauthorizedError});
+                    break;
+                default:
+                    const serverError = {serverError: true};
+                    this.setState({errors: serverError});
+                    break;
+            }
+        }).finally(() => this.setState({loading: false}));
     }
 
     render() {
@@ -81,13 +117,20 @@ class AddExpense extends React.Component {
         const amountInvalid = !expense.amount;
         const dateInvalid = !expense.date;
         const categoryInvalid = !expense.category;
-        const displayErrors = this.state.submitted && (!expense.amount || !expense.date || !expense.category);
+        const displayFormErrors = this.state.submitted && (!expense.amount || !expense.date || !expense.category);
         return (
             <div>
                 <form id='add-expense-form' onSubmit={this.handleSubmit}
-                      className={displayErrors ? 'displayErrors' : ''}>
+                      className={displayFormErrors ? 'displayErrors' : ''}>
                     <h1>Welcome to Slow Sloth Budget</h1>
                     {auth0Client.isAuthenticated() ? <h2>Anonymous users shouldn't see this</h2> : ''}
+                    {this.state.saved ? <span>Expense saved</span> : ''}
+                    {this.state.errors.badRequest ?
+                        <span>The expense you are trying to save contains errors. Please correct them.</span> : ''}
+                    {this.state.errors.unauthorized ?
+                        <span>You are not allowed to save expenses. Please log in.</span> : ''}
+                    {this.state.errors.serverError ?
+                        <span>Your request could not be processed. Internal server error occured.</span> : ''}
                     <AmountInput amount={expense.amount} onAmountChange={this.handleAmountChange}
                                  displayError={amountInvalid} />
                     <DateInput date={expense.date} onDateChanged={this.handleDateChange}
@@ -96,7 +139,9 @@ class AddExpense extends React.Component {
                                    displayError={categoryInvalid} />
                     <DescriptionInput description={expense.description}
                                       onDescriptionChanged={this.handleDescriptionChanged} />
-                    <button type="submit" id='add-expense-button'>Add expense</button>
+                    <button type="submit" id='add-expense-button'>Save expense</button>
+                    {this.state.loading ? <span>Saving...</span> : ''}
+
                 </form>
             </div>
         )
