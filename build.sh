13406ad0b94b7e8e2b83c5a054426a2021860af5
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-09-15 12:53:25 +0200

    Change build and tests order in Travis CI

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 dotnet build -c Release ./src/SlowSlothBudget.Web.sln
-cd ./src/SlowSlothBudget.Web/ClientApp
-npm run build
-cd $TRAVIS_BUILD_DIR
 dotnet test -c Release ./src/SlowSlothBudget.Web.Test/SlowSlothBudget.Web.Test.csproj
 cd ./src/SlowSlothBudget.Web/ClientApp
 npm test
+npm run build
+cd $TRAVIS_BUILD_DIR
