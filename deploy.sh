395c320b717bcfc3ff7a67e940953588c9d8b4ed
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-09-17 21:39:17 +0200

    Fix zip publish folder command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd ./src/SlowSlothBudget.Web/publish
-zip publish
+zip -r publish.zip .
 curl -X POST -u $AZURE_WA_USERNAME:$AZURE_WA_PASSWORD --data-binary @"./publish.zip" https://$AZURE_WA_SITE.scm.azurewebsites.net/api/zipdeploy
