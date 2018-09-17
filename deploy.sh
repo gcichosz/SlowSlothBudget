148a1dd5393ebad271d5502ba04257a0a6c4a3f4
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-09-17 21:28:24 +0200

    Change workdir to publish folder before deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd ./publish
+cd ./src/SlowSlothBudget.Web/publish
 zip publish
 curl -X POST -u $AZURE_WA_USERNAME:$AZURE_WA_PASSWORD --data-binary @"./publish.zip" https://$AZURE_WA_SITE.scm.azurewebsites.net/api/zipdeploy
