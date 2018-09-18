0711552c484d7d43e0eb530991d3ec307bb67547
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-09-18 22:18:00 +0200

    Remove obsolete add expense component tests

--- src/SlowSlothBudget.Web/ClientApp/src/components/AddExpense.test.js
+++ src/SlowSlothBudget.Web/ClientApp/src/components/AddExpense.test.js
@@ -6,22 +6,6 @@ import Auth from '../utils/Auth';
 
 jest.mock('../utils/Auth');
 
-it('does not render authenticated users message to anonymous user', () => {
-    Auth.isAuthenticated.mockImplementationOnce(() => false);
-    const wrapper = shallow(<AddExpense />);
-    const welcome = <h2>Anonymous users shouldn't see this</h2>;
-
-    expect(wrapper.contains(welcome)).toEqual(false);
-});
-
-it('renders authenticated users message to authenticated user', () => {
-    Auth.isAuthenticated.mockImplementationOnce(() => true);
-    const wrapper = shallow(<AddExpense />);
-    const welcome = <h2>Anonymous users shouldn't see this</h2>;
-
-    expect(wrapper.contains(welcome)).toEqual(true);
-});
-
 it('does not accept non amount values in amount input', () => {
     const notAmountInput = 'not-amount';
     const wrapper = mount(<AddExpense />);
