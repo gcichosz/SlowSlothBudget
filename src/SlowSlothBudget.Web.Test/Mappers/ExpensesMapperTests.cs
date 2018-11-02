ace6ebf5af11ac83a96c5806859ecd849ac3bb79
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-11-02 11:56:11 +0100

    Add failing back-end test to ckeck Azure Pipelines

--- src/SlowSlothBudget.Web.Test/Mappers/ExpensesMapperTests.cs
+++ src/SlowSlothBudget.Web.Test/Mappers/ExpensesMapperTests.cs
@@ -125,5 +125,11 @@ namespace SlowSlothBudget.Web.Test.Mappers
 
             Assert.AreEqual(_mappedExpenseDtoSample.Date, actualExpense.Date);
         }
+
+        [Test]
+        public void Failing_Test_To_Check_Azure_Pipelines()
+        {
+            Assert.True(false);
+        }
     }
 }
