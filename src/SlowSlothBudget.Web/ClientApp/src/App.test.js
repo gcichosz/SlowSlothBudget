c057b44354c1e4b4e7eb1f07b8032a3adddd0f19
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-11-02 12:03:45 +0100

    Add failing front-end test to ckeck Azure Pipelines

--- src/SlowSlothBudget.Web/ClientApp/src/App.test.js
+++ src/SlowSlothBudget.Web/ClientApp/src/App.test.js
@@ -5,3 +5,7 @@ import App from './App';
 it('renders without crashing', () => {
     shallow(<App />);
 });
+
+it('failing test to check Azure Pipelines', () => {
+    expect(false).toBe(true);
+});
