e8e2ae86db887f41bd1487ead865ec187441f5e9
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2018-11-02 12:12:11 +0100

    Remove failing front-end test to ckeck Azure Pipelines

--- src/SlowSlothBudget.Web/ClientApp/src/App.test.js
+++ src/SlowSlothBudget.Web/ClientApp/src/App.test.js
@@ -5,7 +5,3 @@ import App from './App';
 it('renders without crashing', () => {
     shallow(<App />);
 });
-
-it('failing test to check Azure Pipelines', () => {
-    expect(false).toBe(true);
-});
