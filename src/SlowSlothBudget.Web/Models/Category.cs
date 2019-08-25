c769f3a0e62215e5749e3592a409494e600fde01
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2019-08-25 20:29:24 +0200

    Add id property to category model and its DTO

--- src/SlowSlothBudget.Web/Models/Category.cs
+++ src/SlowSlothBudget.Web/Models/Category.cs
@@ -1,3 +1,4 @@
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace SlowSlothBudget.Web.Models
@@ -5,6 +6,8 @@ namespace SlowSlothBudget.Web.Models
     [BsonIgnoreExtraElements]
     public class Category
     {
+        public ObjectId Id { get; set; }
+
         public string Name { get; set; }
 
         public int Order { get; set; }
--- src/SlowSlothBudget.Web/Models/Dtos/CategoryDto.cs
+++ src/SlowSlothBudget.Web/Models/Dtos/CategoryDto.cs
@@ -2,6 +2,8 @@ namespace SlowSlothBudget.Web.Models.Dtos
 {
     public class CategoryDto
     {
+        public string Id { get; set; }
+
         public string Name { get; set; }
 
         public int Order { get; set; }
