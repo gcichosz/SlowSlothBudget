7711b362eb7fa14c4ce4a8e1d9bb206a2e4d3695
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2019-08-25 20:30:36 +0200

    Implement category DTO -> category mapping

--- src/SlowSlothBudget.Web/Mappers/CategoriesMapper.cs
+++ src/SlowSlothBudget.Web/Mappers/CategoriesMapper.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Bson;
 using SlowSlothBudget.Web.Models;
 using SlowSlothBudget.Web.Models.Dtos;
 
@@ -12,12 +13,29 @@ namespace SlowSlothBudget.Web.Mappers
             return categories.Select(Map).ToList();
         }
 
+        public IEnumerable<Category> Map(IEnumerable<CategoryDto> categoriesDto, string userId)
+        {
+            return categoriesDto.Select(c => Map(c, userId)).ToList();
+        }
+
         private static CategoryDto Map(Category category)
         {
             return new CategoryDto
             {
+                Id = category.Id.ToString(),
                 Name = category.Name,
                 Order = category.Order
+            };
+        }
+
+        private static Category Map(CategoryDto categoryDto, string userId)
+        {
+            return new Category
+            {
+                Id = string.IsNullOrEmpty(categoryDto.Id) ? new ObjectId() : new ObjectId(categoryDto.Id),
+                Name = categoryDto.Name,
+                Order = categoryDto.Order,
+                OwnerUserId = userId
             };
         }
     }
--- src/SlowSlothBudget.Web/Mappers/ICategoriesMapper.cs
+++ src/SlowSlothBudget.Web/Mappers/ICategoriesMapper.cs
@@ -7,5 +7,6 @@ namespace SlowSlothBudget.Web.Mappers
     public interface ICategoriesMapper
     {
         IEnumerable<CategoryDto> Map(IEnumerable<Category> categories);
+        IEnumerable<Category> Map(IEnumerable<CategoryDto> categoriesDto, string userId);
     }
 }
