4a571dad544d3d5f464230121ef484a31f8549dc
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2019-08-25 20:57:00 +0200

    Order category statistics according to categories order

--- src/SlowSlothBudget.Web/DAL/StatisticsRepository.cs
+++ src/SlowSlothBudget.Web/DAL/StatisticsRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.Options;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
@@ -10,18 +11,20 @@ namespace SlowSlothBudget.Web.DAL
 {
     public class StatisticsRepository : IStatisticsRepository
     {
-        private readonly IMongoCollection<Expense> _collection;
+        private readonly IMongoCollection<Expense> _expensesCollection;
+        private readonly ICategoriesRepository _categoriesRepository;
 
-        public StatisticsRepository(IOptions<MongoDbOptions> options)
+        public StatisticsRepository(IOptions<MongoDbOptions> options, ICategoriesRepository categoriesRepository)
         {
             var client = new MongoClient(options.Value.ConnectionString);
             var database = client.GetDatabase(options.Value.DatabaseName);
-            _collection = database.GetCollection<Expense>(options.Value.ExpensesCollectionName);
+            _expensesCollection = database.GetCollection<Expense>(options.Value.ExpensesCollectionName);
+            _categoriesRepository = categoriesRepository;
         }
 
         public IEnumerable<CategoryStatistics> CalculateUserLastYearAverages(string userId)
         {
-            var userLastYearExpenses = _collection.Aggregate()
+            var userLastYearExpenses = _expensesCollection.Aggregate()
                 .Match(e => e.OwnerUserId == userId && e.Date > DateTime.UtcNow.Date.AddYears(-1));
             var expensesWithDecimalAmountAndEnumMonth = userLastYearExpenses.Project(
                 "{_id:0, category:1, amount: {$convert: {input: \"$amount\", to: \"decimal\"}}, month: {$month: \"$date\"}}");
@@ -32,14 +35,24 @@ namespace SlowSlothBudget.Web.DAL
                 expensesSummedByMonthAndCategory.Group("{_id: \"$_id.category\", amount: {$sum: \"$amount\"}}");
             var userLastYearAverageExpensesByCategory =
                 expensesSummedByCategory.Project(
-                    "{_id:0, name: \"$_id\", amount: {$convert: {input: {$divide: [\"$amount\", 12]}, to: \"string\"}}}").ToList();
+                        "{_id:0, name: \"$_id\", amount: {$convert: {input: {$divide: [\"$amount\", 12]}, to: \"string\"}}}")
+                    .ToList();
             var userLastYearAverageCategoryStatistics = new List<CategoryStatistics>();
             foreach (var category in userLastYearAverageExpensesByCategory)
             {
                 userLastYearAverageCategoryStatistics.Add(BsonSerializer.Deserialize<CategoryStatistics>(category));
             }
 
-            return userLastYearAverageCategoryStatistics;
+            var userCategories = _categoriesRepository.FindUserCategories(userId);
+            return userLastYearAverageCategoryStatistics
+                .Join(userCategories, s => s.Name, c => c.Name,
+                    (categoryStatistics, category) => new
+                        {categoryStatistics.Name, categoryStatistics.Amount, category.Order}).OrderBy(s => s.Order)
+                .Select(s => new CategoryStatistics
+                {
+                    Name = s.Name,
+                    Amount = s.Amount
+                });
         }
     }
 }
